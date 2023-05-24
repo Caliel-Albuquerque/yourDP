a50bbd716f04c5e17679c5c34ee90389e9898f3f
Caliel-Albuquerque <75955275+Caliel-Albuquerque@users.noreply.github.com>
2023-05-24 01:13:52 -0300

    conffs

--- pages/Home/js/localCompany.js
+++ pages/Home/js/localCompany.js
@@ -1,12 +1,12 @@
 const LOCAL_COMPANY = {
-    lat: -7.94624,
-    long: -34.8717056
+    lat: -8.0523285,
+    long: -34.8824806
 }
 
 
 const HOUR_COMPANY = {
-    min: "23:50",
-    max: "23:00"
+    min: "10:00",
+    max: "18:00"
 }
 
 
